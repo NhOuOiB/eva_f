dc0b0bb7704ac39e889e39922889f4fffcdd7471
mike.chang <s33165309000@gmail.com>
2024-07-10 09:55:38 +0800

    [feat] route

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './page/login/Login';
+import Router from './router/Router'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-
   return (
     <>
       <BrowserRouter initialEntries={['/']} initialIndex={0} basename="">
         <Routes>
-          <Route path="/" element={<Login />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/*" element={<Router />}></Route>
         </Routes>
+        <ToastContainer />
       </BrowserRouter>
     </>
   );
