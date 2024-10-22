091c8ce81b8342caf4d2cef899f9d045b68d5263
mike.chang <s33165309000@gmail.com>
2024-10-22 15:03:43 +0800

    feat: Update Login component UI and remove unnecessary code

--- src/page/login/Login.jsx
+++ src/page/login/Login.jsx
@@ -28,15 +28,6 @@ const Login = () => {
       localStorage.setItem('account', res.data.account);
       localStorage.setItem('permission', res.data.permission);
       if (res.data.message === '成功登入') {
-        // toast.success(res.data.message, {
-        //   position: 'top-center',
-        //   autoClose: 3000,
-        // hideProgressBar: true,
-        //   closeOnClick: true,
-        //   pauseOnHover: true,
-        //   draggable: true,
-        //   theme: 'light',
-        // });
       } else {
         toast.error(res.data.message, {
           position: 'top-center',
@@ -71,56 +62,66 @@ const Login = () => {
   return (
     <div className="w-full h-full bg">
       <div className="flex justify-center items-center h-full">
-        <div className="w-full h-full flex flex-col justify-between content">
-          <div>
+        <div className="w-full h-full flex flex-col justify-center items-center content">
+          <div className="absolute left-4 -top-16">
             <img src={logo} alt="logo" />
           </div>
-          <div
-            className={`w-4/5 h-1/3 sm:w-1/2 sm:h-2/5 md:w-1/3 xl:w-1/4 2xl:w-1/5 m-auto bg-white flex justify-center items-center rounded-xl shadow-md transition duration-500 hover:scale-110 hover:shadow-2xl ${Isfocus && 'scale-110 shadow-2xl'}`}
-          >
-            <div className="flex flex-col justify-center items-center gap-4">
-              <div className="flex flex-col">
-                <label htmlFor="account" className="text-left">
-                  帳號
-                </label>
-                <input
-                  type="text"
-                  id="account"
-                  name="account"
-                  value={login.account}
-                  onChange={handleChange}
-                  onFocus={() => setIsFocus(true)}
-                  onBlur={() => setIsFocus(false)}
-                />
-              </div>
-              <div className="flex flex-col">
-                <label htmlFor="password" className="text-left">
-                  密碼
-                </label>
-                <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  value={login.password}
-                  onChange={handleChange}
-                  onKeyDown={(e) => {
-                    if (e.keyCode == 13) {
-                      handleLogin(e);
-                    }
-                  }}
-                  onFocus={() => setIsFocus(true)}
-                  onBlur={() => setIsFocus(false)}
-                />
-              </div>
-              <div
-                className="w-32 h-9 mt-6 flex justify-center items-center rounded border border-green-700 bg-emerald-700 hover:bg-emerald-800 text-white cursor-pointer"
-                onClick={(e) => handleLogin(e)}
-              >
-                登入
+          <div className="w-4/5 h-1/3 sm:w-1/2 sm:h-2/5 md:w-1/3 xl:w-1/4 2xl:w-1/5 flex flex-col justify-center items-center relative">
+            <div className="w-fit text-5xl text-emerald-700 font-bold text-nowrap absolute -top-20">長榮航太梯架設備管理系統</div>
+
+            <div
+              className={`w-full h-full bg-white flex justify-center items-center rounded-xl shadow-md transition duration-500 hover:scale-110 hover:shadow-2xl ${Isfocus && 'scale-110 shadow-2xl'}`}
+            >
+              <div className="flex flex-col justify-center items-center gap-4">
+                <div className="flex flex-col">
+                  <label htmlFor="account" className="text-left">
+                    帳號
+                  </label>
+                  <input
+                    type="text"
+                    id="account"
+                    name="account"
+                    value={login.account}
+                    onChange={handleChange}
+                    onFocus={() => setIsFocus(true)}
+                    onBlur={() => setIsFocus(false)}
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <label htmlFor="password" className="text-left">
+                    密碼
+                  </label>
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={login.password}
+                    onChange={handleChange}
+                    onKeyDown={(e) => {
+                      if (e.keyCode == 13) {
+                        handleLogin(e);
+                      }
+                    }}
+                    onFocus={() => setIsFocus(true)}
+                    onBlur={() => setIsFocus(false)}
+                  />
+                </div>
+                <div
+                  className="w-32 h-9 mt-6 flex justify-center items-center rounded border border-green-700 bg-emerald-700 hover:bg-emerald-800 text-white cursor-pointer"
+                  onClick={(e) => handleLogin(e)}
+                >
+                  登入
+                </div>
               </div>
             </div>
           </div>
-          <div>© {year} Copyright - 岳林工業有限公司</div>
+          <div className="absolute bottom-0">
+            © {year} Copyright -{' '}
+            <a href="https://www.keewing-id.com/" className="text-red-600">
+              KWD
+            </a>
+            岳林工業有限公司
+          </div>
         </div>
       </div>
     </div>
